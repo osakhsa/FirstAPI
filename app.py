312c06d5181fe36b6e279485775590ddf0dbfa60
osakhsa <osakhsa3@gmail.com>
2021-04-19 09:48:05 +0300

    POST for adding an item, solving exceptions with wrong keys in POSTs

--- app.py
+++ app.py
@@ -21,8 +21,12 @@ stores = [
 @app.route('/store', methods=['POST'])
 def create_store():
     data = request.get_json()
+    try:
+        name = data['name']
+    except KeyError:
+        return wfields_response
     store = {
-        'name': data['name'],
+        'name': name,
         'items': []
     }
     stores.append(store)
@@ -44,14 +48,32 @@ def get_stores():
     return jsonify({'stores': stores})
 
 
+# POST /store/<name>/item -> name, price
+@app.route('/store/<string:store_name>/item', methods=["POST"])
+def add_item(store_name):
+    data = request.get_json()
+    for store in stores:
+        if store['name'] == store_name:
+            try:
+                name = data['name']
+                price = data['price']
+            except KeyError:
+                return wfields_response
+            store['items'].append({'name': name, 'price': price})
+            return store
+    return storenf_response
+
+
 # GET /store/<name>/item
 @app.route('/store/<string:name>/item')
 def get_items(name):
     for store in stores:
         if store['name'] == name:
             return jsonify(store['items'])
-    return jsonify({'message': 'store not found'})
+    return jsonify(storenf_response)
 
 
 if __name__ == '__main__':
+    storenf_response = {'message': 'store is not found'}
+    wfields_response = {'message': 'wrong field(s)'}
     app.run(debug=True)
