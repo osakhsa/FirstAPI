86e564199be67f2bba946f1d79dbdfca4ba18fa1
osakhsa <osakhsa3@gmail.com>
2021-04-24 12:40:10 +0300

    Realized: getting one item

--- app_restful.py
+++ app_restful.py
@@ -18,7 +18,26 @@ class ItemList(Resource):
         pass
 
 
+class Item(Resource):
+    def get(self, name):
+        asked_item = tuple(filter(lambda i: i['name'] == name, store))
+        if len(asked_item) == 1:
+            return asked_item[0]
+        elif asked_item == ():
+            return jsonify({'message': "Item wasn't found"})
+        else:
+            return jsonify({'message': "Database error"})
+        # return jsonify({'type': str(type(asked_item)), 'value': str(asked_item)})
+
+    def post(self, name):
+        pass
+
+    def delete(self, name):
+        pass
+
+
 api.add_resource(ItemList, "/items")
+api.add_resource(Item, '/items/<string:name>')
 
 
 if __name__ == "__main__":
