8d9324b4ea5557998197b363074607e208682974
osakhsa <osakhsa3@gmail.com>
2021-04-28 11:32:25 +0300

    Reqparse

--- app_restful.py
+++ app_restful.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify, request
-from flask_restful import Resource, Api
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
 
 store = [
     {'name': "chair", 'price': 1500},
@@ -8,11 +8,12 @@ store = [
 
 app = Flask(__name__)
 api = Api(app)
+parser = reqparse.RequestParser()
 
 
 class ItemList(Resource):
     def get(self):
-        return jsonify(store)
+        return store
 
     def post(self, data):
         pass
@@ -31,7 +32,11 @@ class Item(Resource):
     def post(self, name):
         asked_item = tuple(filter(lambda i: i['name'] == name, store))
         if asked_item == ():
-            new_item = {'name': name, 'price': request.get_json()['price']}
+            parser.add_argument('price')
+            data = parser.parse_args()
+            if not data['price']:
+                return {'message': 'Price isn\'t given'}, 400
+            new_item = {'name': name, 'price': data['price']}
             store.append(new_item)
             return new_item, 201
         else:
@@ -39,11 +44,27 @@ class Item(Resource):
 
     def put(self, name):
         asked_item = tuple(filter(lambda i: i['name'] == name, store))
+        parser.add_argument('price')
+        data = parser.parse_args()
+        if not data['price']:
+            return {'message': 'Price isn\'t given'}, 400
+        if asked_item == ():
+            new_item = {'name': name, 'price': data['price']}
+            store.append(new_item)
+            return new_item, 201
+        elif len(asked_item) > 1:
+            return {'message': "Database error"}, 500
+        else:
+            price = data['price']
+            asked_item[0]['price'] = price
+            return asked_item[0], 201
 
     def delete(self, name):
         asked_item = tuple(filter(lambda i: i['name'] == name, store))
         if asked_item == ():
             return {'message': "Item wasn't found"}, 405
+        elif len(asked_item) > 1:
+            return {'message': "Database error"}, 500
         else:
             store.remove(asked_item[0])
             return {}, 204
